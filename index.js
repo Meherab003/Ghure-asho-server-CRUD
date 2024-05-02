f9269125f220df4eb6d0503bae44a92adb3a335a
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-05-02 12:41:32 +0600

    update and delete added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,7 @@ app.use(express.json());
 
 //========MongoDB==========
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqfu1nq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,40 +19,78 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //Database declaration
-    const spotsCollection = client.db('spotsDB').collection('spots');
+    const spotsCollection = client.db("spotsDB").collection("spots");
 
-    app.get('/spots', async(req, res) => {
+    app.get("/spots", async (req, res) => {
       const cursor = spotsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get('/spots/:id', async(req, res) => {
+    app.get("/spots/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await spotsCollection.findOne(query);
-      res.send(result); 
-    })
+      res.send(result);
+    });
 
-    app.post('/spots', async(req, res) => {
+    app.get("/mylist/:email", async (req, res) => {
+      const Email = req.params.email;
+      const query = { email: Email };
+      const cursor = spotsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.put("/spots/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedSpot = req.body;
+      const spot = {
+        $set: {
+          photo: updatedSpot.photo,
+          name: updatedSpot.name,
+          country: updatedSpot.country,
+          location: updatedSpot.location,
+          description: updatedSpot.description,
+          average: updatedSpot.average,
+          seasonality: updatedSpot.seasonality,
+          time: updatedSpot.time,
+          visitor: updatedSpot.visitor,
+        },
+      };
+      const result = await spotsCollection.updateOne(filter, spot);
+      res.send(result);
+    });
+
+    app.post("/spots", async (req, res) => {
       const newSpot = req.body;
       console.log(newSpot);
-      const result = await spotsCollection.insertOne(newSpot)
+      const result = await spotsCollection.insertOne(newSpot);
+      res.send(result);
+    });
+
+    app.delete('/spots/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await spotsCollection.deleteOne(query);
       res.send(result);
     })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -60,12 +98,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send("Ghure Asho Server is Running like a fox");
-})
+app.get("/", (req, res) => {
+  res.send("Ghure Asho Server is Running like a fox");
+});
 
 app.listen(port, () => {
-    console.log(`Ghure Asho is running on port: ${port}`)
-})
+  console.log(`Ghure Asho is running on port: ${port}`);
+});
